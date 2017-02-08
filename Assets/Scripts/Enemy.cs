ed18f2e9480b8598e53e93fe24fb0277fb319097
Mufurr <Mufurr@gmail.com>
2017-02-08 22:29:38 +0100

    Defining Enemy Patterns
    EnemyManager will give instructions to the enemy, such as where to move and where to shoot. Enemies should be able to find the shortest path to a given target place, and to shoot to a given target position.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    public Rigidbody rig;
+
+    public Vector3 Target { private get; set; }
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Mathf.Abs(rig.transform.position.x - Target.x + rig.transform.position.y - Target.y) > 10) { }
+	}
+
+    private void Move() { }
+}
---
+++ Assets/Scripts/EnnemieManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnnemieManager : MonoBehaviour {
+
+    private List<Enemy> ListEnemy = new List<Enemy> { };
+
+    public Enemy test;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
