a959ad5387e730baf96076962b3e6e05cd006746
Mufurr <Mufurr@gmail.com>
2017-02-09 01:04:30 +0100

    Some changes on Enemy.cs
    Still doesn't work (nav mesh prob)

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,21 +1,25 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Enemy : MonoBehaviour {
 
     public Rigidbody rig;
 
+    public Rigidbody Player;
+
     public Vector3 Target { private get; set; }
+
+    public float speed;
+
+    public NavMeshAgent agent;
     
 	// Use this for initialization
 	void Start () {
-	
-	}
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(Player.transform.position);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Mathf.Abs(rig.transform.position.x - Target.x + rig.transform.position.y - Target.y) > 10) { }
 	}
-
-    private void Move() { }
 }
