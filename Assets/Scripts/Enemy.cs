24627b2e3887a3cc2027c2b591129e4d91a21744
Mufurr <Mufurr@gmail.com>
2017-02-15 19:34:37 +0100

    Enemy AI fixed and now consider line of sight
    Now the Enemy AI will stop following the player if it is too close AND can see the player. However, it doesn't consider line of sight for other kinds of targets, like simple positions. NOTE: The AI will have a strange behavior if both the enemy and the mech are standing in the opposite sides of a really, really thin wall. But the wall has to be so thin that's it's irrevelant. Also, some constants will have to adjusted depending on both player's and enemy's size.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,24 +3,56 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour {
 
-	public Rigidbody rig;
+    public Rigidbody rig;
 
-	public Vector3 Target { private get; set; }
+    public Vector3 Target { private get; set; }
+    public float distance;
+    public bool TargetPlayer;
 
-	public float speed;
+    public float speed;
+    public float safeDistance; 
 
-	public NavMeshAgent agent;
-
+    public NavMeshAgent agent;
+    
 	// Use this for initialization
-	void Start () 
-	{
-		agent = GetComponent<NavMeshAgent>();
-		Target = rig.transform.position;
-	}
-
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        Target = rig.transform.position;
+    }
+	
 	// Update is called once per frame
 	void LateUpdate ()
-	{
-		agent.SetDestination(Target);
-	}
+    {
+        if (TargetPlayer)
+        {
+            MoveToPlayer();
+        }
+        else
+        {
+            agent.SetDestination(Target);
+        }
+    }
+
+    void MoveToPlayer()
+    {
+        RaycastHit Hit;
+        if (Mathf.Abs(Target.x - rig.transform.position.x) + Mathf.Abs(Target.z - rig.transform.position.z) < safeDistance)
+        {
+            rig.rotation = Quaternion.LookRotation(Target - rig.position);
+            rig.velocity = rig.transform.TransformDirection(Vector3.back);
+            agent.ResetPath();
+        }
+        else if (Mathf.Abs(Target.x - rig.transform.position.x) + Mathf.Abs(Target.z - rig.transform.position.z) > distance
+             || !(Physics.Raycast(rig.position, rig.transform.TransformDirection(Vector3.forward), out Hit) && Hit.transform.position.x == Target.x
+             && Hit.transform.position.y == Target.y))
+        {
+            agent.SetDestination(Target);
+        }
+        else
+        {
+            rig.rotation = Quaternion.LookRotation(Target - rig.position);
+            rig.velocity = Vector3.zero;
+            agent.ResetPath();
+        }
+    }
 }
