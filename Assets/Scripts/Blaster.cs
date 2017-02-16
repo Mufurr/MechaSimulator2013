9bf9eddd7a168da443b07850fa105625224988c8
Mufurr <Mufurr@gmail.com>
2017-02-16 14:24:11 +0100

    Blaster and Missile added
    I added both scripts and prefab.

---
+++ Assets/Scripts/Blaster.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blaster : Enemy {
+
+    private int reload;
+    public int reloadRate;
+
+    public GameObject Missile;
+
+    protected override void Fire()
+    {
+        RaycastHit Hit;
+        if (reload == 0)
+        {
+            if (Physics.Raycast(rig.position, rig.transform.TransformDirection(Vector3.forward), out Hit)
+                && Hit.collider.name == "Mech")
+            {
+                Instantiate(Missile, rig.position, rig.rotation);
+                reload = reloadRate;
+            }
+        }
+        else {
+            reload--;
+        }
+    }
+}
