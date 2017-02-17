61f0e2010d9200e173569ecf1357e40085236255
Mufurr <Mufurr@gmail.com>
2017-02-17 01:39:07 +0100

    The spawner and lots of little changes
    I've worked a lot without commiting, here's everything at once.

---
+++ Scripts/Blaster.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blaster : Enemy {
+
+    private int reload;
+    public int reloadRate;
+
+    public GameObject Missile;
+
+    public override void Start()
+    {
+        level = 1;
+    }
+
+    protected override void Fire()
+    {
+        RaycastHit Hit;
+        if (reload == 0)
+        {
+            if (Physics.Raycast(rig.position, rig.transform.TransformDirection(Vector3.forward), out Hit)
+                && Hit.collider.tag == "Player")
+            {
+                Instantiate(Missile, rig.position, rig.rotation);
+                reload = reloadRate;
+            }
+        }
+        else {
+            reload--;
+        }
+    }
+}
---
+++ Scripts/Missile.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Missile : MonoBehaviour {
+
+    public Transform Trans;
+
+    public float speed;
+    public float delta;
+    public int LifeTime;
+    public bool Enemy;
+
+	void Start () {
+	}
+
+
+	void FixedUpdate ()
+    {
+        float angle = Trans.rotation.eulerAngles.y - 45;//For some weird reasons, the missile won't go in the right directin without this "- 45"
+        Vector3 pos = Trans.position;
+        float rad = ToRad(angle);
+        pos.z += speed * (Mathf.Cos(rad) + Mathf.Cos((Mathf.PI / 2) + rad));
+        pos.x += speed * (Mathf.Cos((Mathf.PI / 2) - rad) + Mathf.Cos(rad));
+        Trans.position = pos;
+        if (LifeTime == 0)
+        {
+            Destroy(Trans.gameObject);
+        }else {
+            LifeTime--;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (Enemy && other.tag == "Player") {
+            Destroy(Trans.gameObject);
+            Debug.Log("A missile hit the player!");
+        }
+        if (!Enemy && other.tag == "Enemy") {
+            Destroy(Trans.gameObject);
+            Debug.Log("A missile hit an enemy!");
+        }
+        if (other.tag == "Obstacle") {
+            Destroy(Trans.gameObject);
+            Debug.Log("A missile hit an obstacle!");
+        }
+    }
+
+    float ToRad(float angle)
+    {
+        return ((Mathf.PI) / 180) * angle;
+    }
+}
