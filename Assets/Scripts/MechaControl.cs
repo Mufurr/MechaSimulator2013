ad95dd9cd8b72f678b8e85ead8e2f4b2d1c55977
Foupi <pierre.foucart@epita.fr>
2017-02-19 16:54:51 +0100

    MechaControl 2.0
    Now he can shoot.
    Almost.
    Gonna fix it.

--- Assets/Scripts/MechaControl.cs
+++ Assets/Scripts/MechaControl.cs
@@ -6,6 +6,7 @@ public class MechaControl : MonoBehaviour
 
 	public Rigidbody rig = new Rigidbody();
 	private Vector3 pos;
+	public Missile missile;
 	public float speed;
 	public float rotation;
 	private float angle;
@@ -17,14 +18,16 @@ public class MechaControl : MonoBehaviour
 	{
 		Move ();
 		Rotation ();
+		Shoot ();
 	}
 
 	void Move ()
 	{
 		pos = rig.transform.position;
 		float rad = ((Mathf.PI) / 180) * angle;
-		pos.z += speed * (Input.GetAxis ("Horizontal2") * Mathf.Cos (rad) + Input.GetAxis ("Horizontal") * Mathf.Cos ((Mathf.PI / 2) + rad ));
-		pos.x += speed * (Input.GetAxis ("Horizontal2") * Mathf.Cos ((Mathf.PI / 2) - rad) + Input.GetAxis("Horizontal") * Mathf.Cos (rad));
+		float cos = Mathf.Cos(rad);
+		pos.z += speed * (Input.GetAxis ("Horizontal2") * cos + Input.GetAxis ("Horizontal") * Mathf.Cos ((Mathf.PI / 2) + rad ));
+		pos.x += speed * (Input.GetAxis ("Horizontal2") * Mathf.Cos ((Mathf.PI / 2) - rad) + Input.GetAxis("Horizontal") * cos);
 		rig.transform.position = pos;
 	}
 
@@ -32,5 +35,10 @@ public class MechaControl : MonoBehaviour
 	{
 		angle += rotation * Input.GetAxis("Rotation");
 		rig.transform.rotation = Quaternion.Euler(0, angle, 0);
+	}
+
+	void Shoot()
+	{
+		Instantiate (missile, rig.position, rig.rotation);
 	}
 }
