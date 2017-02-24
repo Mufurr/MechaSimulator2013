d4cb76a049007733591fb03bde9b0bace6858751
Foupi <pierre.foucart@epita.fr>
2017-02-25 00:14:08 +0100

    MechaControl 3.1
    The mecha now has hp, and two tankers. When he moves, he uses the fuel of one of them, while the other one automatically fills itself.
    /!\ You have to set the "health" and "tankers" variables at an other value than 0, or else the mecha won't do anything.

--- Assets/Scripts/MechaControl.cs
+++ Assets/Scripts/MechaControl.cs
@@ -5,18 +5,29 @@ public class MechaControl : MonoBehaviour
 {
 
 	public Rigidbody rig = new Rigidbody();
+	public int health;
+	public int tankers;
+	private bool current_tanker;
+	private int tanker1;
+	private int tanker2;
 	private Vector3 pos;
-	public Missile missile;
-	public float speed;
-	public float rotation;
+	public float movement_speed;
+	public float rotation_speed;
 	private float angle;
+	public Missile missile;
+	public int reload;
 	private int reloadMissile = 0;
 
 	void Start () 
-	{}
+	{
+		tanker1 = tankers;
+		tanker2 = tankers;
+	}
 
 	void Update () 
 	{
+		Is_Dead ();
+		TankersManager ();
 		Move ();
 		Rotation ();
 		reloadMissile--;
@@ -24,17 +35,27 @@ public class MechaControl : MonoBehaviour
 
 	void Move ()
 	{
-		pos = rig.transform.position;
-		float rad = ((Mathf.PI) / 180) * angle;
-		float cos = Mathf.Cos(rad);
-		pos.z += speed * (Input.GetAxis ("Horizontal2") * cos + Input.GetAxis ("Horizontal") * Mathf.Cos ((Mathf.PI / 2) + rad ));
-		pos.x += speed * (Input.GetAxis ("Horizontal2") * Mathf.Cos ((Mathf.PI / 2) - rad) + Input.GetAxis("Horizontal") * cos);
-		rig.transform.position = pos;
+		if ((current_tanker && tanker1 > 0) || (!current_tanker && tanker2 > 0))
+		{
+			pos = rig.transform.position;
+			float rad = ((Mathf.PI) / 180) * angle;
+			float cos = Mathf.Cos(rad);
+			pos.z += movement_speed * (Input.GetAxis ("Horizontal2") * cos + Input.GetAxis ("Horizontal") * Mathf.Cos ((Mathf.PI / 2) + rad ));
+			pos.x += movement_speed * (Input.GetAxis ("Horizontal2") * Mathf.Cos ((Mathf.PI / 2) - rad) + Input.GetAxis("Horizontal") * cos);
+			rig.transform.position = pos;
+			if (Input.GetAxis ("Horizontal2") != 0 || Input.GetAxis("Horizontal") != 0)
+			{
+				if (current_tanker)
+					tanker1--;
+				else
+					tanker2--;
+			}
+		}
 	}
 
 	void Rotation()
 	{
-		angle += rotation * Input.GetAxis("Rotation");
+		angle += rotation_speed * Input.GetAxis("Rotation");
 		rig.transform.rotation = Quaternion.Euler(0, angle, 0);
 	}
 
@@ -43,7 +64,37 @@ public class MechaControl : MonoBehaviour
 		if (reloadMissile <= 0)
 		{
 			Instantiate (missile, rig.position, rig.rotation);
-			reloadMissile = 50;
+			reloadMissile = reload;
 		}
+	}
+
+	public void Is_Shot()
+	{
+		health -= 25;
+	}
+
+	void TankersManager ()
+	{
+		if (current_tanker)
+		{
+			if (tanker2 < tankers)
+				tanker2++;
+			if (tanker1 <= 0)
+				current_tanker = false;
+			}
+		else
+		{
+			if (tanker1 < tankers)
+				tanker1++;
+			if (tanker2 <= 0)
+				current_tanker = true;
+		}
+		Debug.Log ("tanker 1: " + tanker1.ToString() + ", tanker 2: " + tanker2.ToString());
+	}
+
+	void Is_Dead()
+	{
+		if (health <= 0)
+			Destroy (this);
 	}
 }
