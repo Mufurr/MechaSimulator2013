e4c1173679c306f8d7680a56610d0d97696b6a1d
Foupi <pierre.foucart@epita.fr>
2017-02-25 12:25:02 +0100

    Reloading point (script)
    Don't forget to get the prefab

---
+++ Assets/Scripts/ReloadingPoint.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReloadingPoint : MonoBehaviour {
+
+	public Collider Coll;
+	public MechaControl Mecha;
+	private int heal;
+	private int cooldown;
+
+	void Start () 
+	{
+		heal = 50;
+		cooldown = 120;
+	}
+	
+	void Update () 
+	{
+		cooldown--;	
+	}
+}
