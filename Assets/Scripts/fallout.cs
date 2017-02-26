69086b1a14a316af57906377133d3ee5ef5ac69c
Foupi <pierre.foucart@epita.fr>
2017-02-26 03:29:35 +0100

    Fallout
    The buildings will fall after 30 explosions. Drag&drop their RigidBody in the "Rig" field.

--- Assets/Scripts/fallout.cs
+++ Assets/Scripts/fallout.cs
@@ -1,34 +1,43 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-public class fallout : MonoBehaviour
+public class Fallout : MonoBehaviour
 {
 
-    public Rigidbody rig = new Rigidbody();
-    private int health;
-    private Vector3 pos;
+	public Rigidbody rig = new Rigidbody();
+	private int health;
+	private Vector3 pos;
 
-    void Start()
-    {
-        health = 300;
-    }
+	void Start()
+	{
+		pos = rig.transform.position;
+		health = 300;
+	}
 
-    void fixUpdate()
-    {
-        if ((health <= 0) && (transform.position.y > -0.5f))
-        {
-            float g = (transform.position.y - 0.01f);
-            SetTransformX(g);
-        }
-    }
+	void FixedUpdate()
+	{
+		if (IsDead ())
+			Fall ();
+	}
 
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Explosion")
-            health -= 10;
-    }
-    void SetTransformX(float n)
-    {
-        transform.position = new Vector3(transform.position.x, n , transform.position.z);
-    }
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Explosion")
+			health -= 10;
+	}
+
+	bool IsDead ()
+	{
+		return health <= 0;
+	}
+
+	void Fall ()
+	{
+		pos = rig.transform.position;
+		if (pos.y != -25)
+		{
+			pos.y -= 0.10F;
+		}
+		rig.transform.position = pos;
+	}
 }
