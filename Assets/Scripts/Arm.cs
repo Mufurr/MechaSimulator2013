d998863e1dc3d47de58a0b99e05d8b8261e303a4
Foupi <pierre.foucart@epita.fr>
2017-04-21 10:32:05 +0200

    Arms
    They're now complete!

---
+++ Assets/Scripts/Arm.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arm : MonoBehaviour {
+
+	public bool rightArm;
+	public Shoulder shoulder;
+	public Elbow elbow;
+	public int health { get; private set; }
+	private float max;
+	private float min;
+
+	void Start () 
+	{
+		health = 100;
+		if (rightArm)
+		{
+			min = -90;
+			max = 0;
+		}
+		else
+		{
+			min = 0;
+			max = 90;
+		}
+	}
+	
+	public void Move () 
+	{
+		if (health >= 0) 
+		{
+			shoulder.Move (min, max);
+			if ((shoulder.angley <= min) || (shoulder.angley >= max)) 
+			{
+				elbow.Move (min, max);
+			}
+		}
+	}
+
+	/*void OnTriggerEnter (Collider other)
+	{
+		Physics.IgnoreLayerCollision (8, 10);
+		if (other.tag == "Explosion") {
+			health -= 10;
+			Debug.Log ("Arm damaged");
+		}
+	}*/
+}
---
+++ Assets/Scripts/Elbow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Elbow : MonoBehaviour 
+{
+
+	private Transform Trans;
+	private float angley;
+
+	void Start () 
+	{
+		Trans = GetComponent<Transform> ();
+		angley = Trans.localRotation.eulerAngles.y;
+		if (angley == 315)
+			angley = -45;
+	}
+
+	public void Move (float min, float max)
+	{
+		angley += 2 * Input.GetAxis ("Horizontal");
+		if (angley < min)
+			angley = min;
+		if (angley > max)
+			angley = max;
+		Trans.localRotation = Quaternion.Euler (0, angley, 90);
+	}
+}
