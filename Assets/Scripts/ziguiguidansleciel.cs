da8011fda1ea7f28b0cbf7e8786dff28672b05f0
Azenstrange <edouard.eisenfisz@epita.fr>
2017-04-21 09:58:16 +0200

    script pour les toits
    pour qu'ils volent comme des oiseaux

---
+++ Assets/Scripts/ziguiguidansleciel.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ziguiguidansleciel : MonoBehaviour {
+
+    public Rigidbody rig = new Rigidbody();
+    private int health;
+    private Vector3 pos;
+
+    void Start()
+    {
+        pos = rig.transform.position;
+        health = 300;
+    }
+
+    void FixedUpdate()
+    {
+        if (IsDead())
+            Fall();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Explosion")
+            health -= 10;
+    }
+
+    bool IsDead()
+    {
+        return health <= 0;
+    }
+
+    void Fall()
+    {
+        pos = rig.transform.position;
+        if (pos.y >= 1000)
+        {
+            pos.y += 0.2F;
+        }
+        rig.transform.position = pos;
+    }
+}
